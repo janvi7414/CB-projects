95b09693c3942f639d57f6e20005d485fc9f9fa2
janvi7114 <chichuddejanvi@gmail.com>
2025-01-12 21:13:48 +0530

    created a function to trim unwanted mid spaces in the name

--- name_editor.cpp
+++ name_editor.cpp
@@ -50,15 +50,45 @@ string trim_space(string name)
     //name. returns size which is stored in size_t datatyped var//
     size_t start_index = name.find_first_not_of(" ");
     //find_first_not_of returns a pointer pointing first index which is not space//
-    cout<<endl<<"start_index: "<<name[start_index];
+    //cout<<endl<<"start_index: "<<name[start_index];//
     size_t end_index = name.find_last_not_of(" ");
     //find_first_not_of returns a pointer pointing last index which is not space//
-    cout<<endl<<"end_index: "<<name[end_index];
+    //cout<<endl<<"end_index: "<<name[end_index];//
 
     /*substr(start, length) extracts a portion of the string starting from start
     and having a length of end - start + 1*/
     return name.substr( start_index , end_index - start_index + 1 );
 
+}
+
+//creating a function to edit spaces between name//
+string trim_between_spaces(string name)
+{
+    string trimed_name="";
+    bool space_found=false;
+    for(char ch: name)
+    {
+        //if the char is not space then simply add alphabet to result//
+        if(ch!=' ')
+        {
+            trimed_name+=ch;
+            space_found=false;
+
+        }else
+        {
+            //when ch is a space//
+            //if it is valid i.e single space between name//
+            if(!space_found)//!space_found indicates the space is the first space found which is valid//
+            {
+                //add space//
+                trimed_name+=ch;
+            space_found=true;
+            }//for else part when space_found is true it will simply increament ch//
+        }
+    }
+    return trimed_name;
+    /*this function will not edit tailing space thus call trim_space function afterwards to
+    trim tailing spaces*/
 }
 
 int main()
@@ -71,7 +101,7 @@ int main()
         if(valid_name(name))
             {
                 //to convert the inputed name in proper formate i.e. first letter captial//
-                string edited_name = trim_space(capitalize_name(name));
+                string edited_name = trim_space(trim_between_spaces(capitalize_name(name)));
                 cout<<endl<<edited_name;
                 //clearing string//
                 name.clear();
